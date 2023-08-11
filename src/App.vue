0aa23a533dc5bc866f873e9a822b0362e2029a88
amberyj25 <amber20240@gmail.com>
2023-08-11 22:22:21 +0800

    串接API & 資料呈現

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <input type="text" id="searchInput" v-model="searchText" />
-  <button @click="searchItunes(searchText)">Search</button>
+  <div id="app">
+    <input type="text"
+      class="singerName"
+      v-model="searchText"
+      placeholder="輸入歌手名稱(ex.taylors)"
+      @keydown.enter="searchItunes(searchText)" />
+    <button @click="searchItunes(searchText)">查詢</button>
+    <div v-for="album in albums.data.results" :key="album.artistId">
+      <AlbumsData :album="album"></AlbumsData>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, reactive } from 'vue'
+import { ItunesTypes } from '@/types/ItunesTypes'
+import AlbumsData from '@/components/AlbumsData.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    AlbumsData
+  },
   setup () {
-    const albums = reactive({ data: {} })
+    const albums = reactive<{ data: ItunesTypes }>({ data: {} })
     const searchText = ref('')
-    const searchItunes = (search: string) => {
-      console.log('data', albums)
+    const searchItunes = async (search: string) => {
+      const value = await fetch(`https://itunes.apple.com/search?term=${search}&entity=album`)
+      const valueToJSON = await value.json()
+      albums.data = valueToJSON
     }
 
     return {
@@ -23,3 +39,10 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.singerName {
+  width: 200px;
+  height: 20px;
+}
+</style>
---
+++ src/components/AlbumsData.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>專輯名稱: {{ album.collectionName }}</h2>
+    <img :src="album.artworkUrl100" alt="picture"/>
+    <h4>價格: {{ album.collectionPrice }}</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AlbumsData',
+  props: {
+    album: {
+      type: Object,
+      required: true
+    }
+  },
+  setup () {
+    return {}
+  }
+})
+</script>
