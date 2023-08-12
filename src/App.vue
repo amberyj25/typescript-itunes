999ded51f62f611f6c8d7b4e2c5c0c224ce34cb4
amberyj25 <amber20240@gmail.com>
2023-08-12 22:27:45 +0800

    使用bootstrap grid ＆ 加入try catch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-    <input type="text"
-      class="singerName"
-      v-model="searchText"
-      placeholder="輸入歌手名稱(ex.taylors)"
-      @keydown.enter="searchItunes(searchText)" />
-    <button @click="searchItunes(searchText)">查詢</button>
-    <div v-for="album in albums.data.results" :key="album.artistId">
-      <AlbumsData :album="album"></AlbumsData>
+    <div class="container">
+      <div class="search-box">
+        <input type="text"
+          class="singer-name"
+          v-model="searchText"
+          placeholder="輸入歌手名稱(ex.taylors)"
+          @keydown.enter="searchItunes(searchText)" />
+        <button type="button" class="btn btn-primary" @click="searchItunes(searchText)">查詢</button>
+      </div>
+      <div class="album-title">
+        <div>歌手專輯(輸入歌手後呈現)</div>
+      </div>
+      <div class="row">
+        <div v-for="album in albums.data.results" :key="album.artistId" class="album-box col-3">
+          <AlbumsData :album="album"></AlbumsData>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -26,9 +35,13 @@ export default defineComponent({
     const albums = reactive<{ data: ItunesTypes }>({ data: {} })
     const searchText = ref('')
     const searchItunes = async (search: string) => {
-      const value = await fetch(`https://itunes.apple.com/search?term=${search}&entity=album`)
-      const valueToJSON = await value.json()
-      albums.data = valueToJSON
+      try {
+        const value = await fetch(`https://itunes.apple.com/search?term=${search}&entity=album`)
+        const valueToJSON = await value.json()
+        albums.data = valueToJSON
+      } catch (err) {
+        console.log('catch', err)
+      }
     }
 
     return {
@@ -41,8 +54,33 @@ export default defineComponent({
 </script>
 
 <style>
-.singerName {
+.container {
+  width: 90%;
+}
+.search-box {
+  display: flex;
+  height: 60px;
+  justify-content: center;
+  align-items: center;
+}
+.singer-name {
+  display: block;
   width: 200px;
-  height: 20px;
+  height: 30px;
+  margin: 0 5px 0 0;
+}
+.btn {
+  display: block;
+  width: 75px;
+  height: 35px;
+}
+.album-title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 5px 0 15px 0;
+}
+.album-box {
+  border: 2px solid black;
 }
 </style>
--- src/components/AlbumsData.vue
+++ src/components/AlbumsData.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <h2>專輯名稱: {{ album.collectionName }}</h2>
-    <img :src="album.artworkUrl100" alt="picture"/>
-    <h4>價格: {{ album.collectionPrice }}</h4>
+  <div class="album">
+    <div class="album-name-title">專輯名稱: </div>
+    <div class="album-name">{{ album.collectionName }}</div>
+    <div class="album-img-box">
+      <img :src="album.artworkUrl100" alt="img" class="album-img"/>
+    </div>
   </div>
 </template>
 
@@ -22,3 +24,26 @@ export default defineComponent({
   }
 })
 </script>
+
+<style>
+.album {
+  margin: 15px;
+}
+.album-name-title {
+  font-size: 20px;
+}
+.album-name {
+  height: 28px;
+  font-size: 16px;
+}
+.album-img-box {
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.album-img {
+  width: 125px;
+  height: 100px;
+}
+</style>
